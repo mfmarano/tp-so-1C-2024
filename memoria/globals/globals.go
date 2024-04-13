1fff4853ff50dbba99a5935d49735cc2a6420f3e
Elias Bulesevich <ebulesevich@frba.utn.edu.ar>
2024-04-13 18:47:09 -0300

    configuración logger e inicio de configuración

--- memoria/globals/globals.go
+++ memoria/globals/globals.go
@@ -1,6 +1,11 @@
 package globals
 
 type ModuleConfig struct {
+	Port            int    `json:"port"`
+	MemorySize      int    `json:"memory_size"`
+	PageSize        int    `json:"page_size"`
+	InstructionPath string `json:"instruction_path"`
+	DelayResponse   int    `json:"delay_response"`
 }
 
 var Config *ModuleConfig
--- memoria/memoria.go
+++ memoria/memoria.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/sisoputnfrba/tp-golang/memoria/globals"
+	"github.com/sisoputnfrba/tp-golang/memoria/handlers"
+	"github.com/sisoputnfrba/tp-golang/utils/configs"
+	"github.com/sisoputnfrba/tp-golang/utils/logs"
+)
+
 func main() {
+
+	// =============
+	// Configuración
+	// =============
+
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	logs.ConfigurarLogger(filepath.Join(path, "memoria.log"))
+
+	globals.Config = configs.IniciarConfiguracion(filepath.Join(path, "config.json"), &globals.ModuleConfig{}).(*globals.ModuleConfig)
+
+	if globals.Config == nil {
+		log.Fatalf("No se pudo cargar la configuración")
+	}
+
+	// ========
+	// Interfaz
+	// ========
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/mensaje", handlers.RecibirMensaje)
+
+	// ======
+	// Inicio
+	// ======
+
+	log.Printf("El módulo memoria está a la escucha en el puerto %d", globals.Config.Port)
+
+	err = http.ListenAndServe(fmt.Sprintf(":%d", globals.Config.Port), mux)
+	if err != nil {
+		panic(err)
+	}
 
 }
