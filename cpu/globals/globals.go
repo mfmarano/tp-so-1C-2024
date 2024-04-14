885df3f4ba89484c1d8da50d1bec0ec271a1cfb7
larramendyjuan <82283258+larramendyjuan@users.noreply.github.com>
2024-04-14 19:26:49 -0300

    feat: modulo cpu

--- cpu/globals/globals.go
+++ cpu/globals/globals.go
@@ -1,6 +1,11 @@
 package globals
 
 type ModuleConfig struct {
+	Port             int    `json:"port"`
+	IpMemory         string `json:"ip_memory"`
+	PortMemory       int    `json:"port_memory"`
+	NumberFellingTlb int    `json:"number_felling_tlb"`
+	AlgorithmTlb     string `json:"algorithm_tlb"`
 }
 
 var Config *ModuleConfig
