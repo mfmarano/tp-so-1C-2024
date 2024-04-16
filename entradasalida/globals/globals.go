03be7edb9721edee248734a1b8db4ec4661720e2
mgratz@frba.utn.edu.ar <mgratz@frba.utn.edu.ar>
2024-04-16 14:05:03 -0300

    Completo Configuracion en globals.go

--- entradasalida/globals/globals.go
+++ entradasalida/globals/globals.go
@@ -1,6 +1,16 @@
 package globals
 
 type ModuleConfig struct {
+	Port             int    `json:"port"`
+	Type             string `json:"type"`
+	UnitWorkTime     int    `json:"unit_work_time"`
+	IpKernel         int    `json:"ip_kernel"`
+	PortKernel       string `json:"port_kernel"`
+	IpMemory         string `json:"ip_memory"`
+	PortMemory       int    `json:"port_memory"`
+	DialFSPath       string `json:"dialfs_path"`
+	DialFSBlockSize  int    `json:"dialfs_block_size"`
+	DialFSBlockCount int    `json:"dialfs_block_count"`
 }
 
 var Config *ModuleConfig
